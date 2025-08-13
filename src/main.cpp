5f9964e612f065a131914bbf1681f8e6a62610a9
Monochrome PIe <popularmmos2548@gmail.com>
2025-08-13 16:35:17 +0700

    Refactor robotic arm control code: streamline servo initialization and enhance CCD algorithm for improved performance, I create new.cpp because main.cpp is too ass

--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,25 @@
 #include <Servo.h>
 #include "Vector2D.h"
 
+int MIN_MICROSEC = 500;
+int MAX_MICROSEC = 2500;
+
 // lengths of the robotic arm segments in millimeters
 double l1 = 105; // length of link 1 controled by servo 2
 double l2 = 125; // length of link 2 controled by servo 3
 double l3 = 175; // length of link 3 controled by servo 4
 
-Servo servo_1; // servo for base rotation
-Servo servo_2; // servo for first link joint
-Servo servo_3; // servo for second link joint
-Servo servo_4; // servo for third link joint
-Servo servo_5; // servo for end effector rotation
-Servo servo_6; // servo for end effector gripper
-
-Servo servos[6] = {servo_1, servo_2, servo_3, servo_4, servo_5, servo_6};
-
 // when initializing the servos the arm should be in the upright position with open gripper
-double initializingTheta_1 = 90;
-double initializingTheta_2 = 90;
-double initializingTheta_3 = 90;
-double initializingTheta_4 = 90;
-double initializingTheta_5 = 0;
-double initializingTheta_6 = 90;
+Servo servos[6];
+int initializingThetas[6] = {90,90,90,90,0,90};
+int attachPin[6] = {5,4,0,14,12,13}; //D1,D2,D3,D5,D6,D7
 
-double initializingThetas[6] = {
-    initializingTheta_1,
-    initializingTheta_2,
-    initializingTheta_3,
-    initializingTheta_4,
-    initializingTheta_5,
-    initializingTheta_6
-};
 
-void setup()
-{
-
-  // Lolin ESP8266 NodeMCU V3 pinout
-  servo_1.attach(5, 500, 2500);  // D1
-  servo_2.attach(4, 500, 2500);  // D2
-  servo_3.attach(0, 500, 2500);  // D3
-  servo_4.attach(14, 500, 2500); // D5
-  servo_5.attach(12, 500, 2500); // D6
-  servo_6.attach(13, 500, 2500); // D7
-
+// Lolin ESP8266 NodeMCU V3
+void setup() {
   Serial.begin(9600);
-
-  for (int i = 0; i < 6; i++)
-  {
+  for (int i = 0; i < 6; i++){
+    servos[i].attach(attachPin[i], MIN_MICROSEC , MAX_MICROSEC);
     servos[i].write(initializingThetas[i]);
   }
 
@@ -76,27 +49,14 @@ void loop(){
     double VLA_ARM_PROPORTIONAL_GAIN = 1; // proportional gain for VLA arm movement input, ADJUSTABLE — based on dx dz
     double VLA_BASE_ROTATION_GAIN = 10; // proportional gain for VLA base rotation input, ADJUSTABLE — based on dy
     double VLA_GRIPPER_ROTATION_GAIN = 10; // proportional gain for VLA gripper rotation input, ADJUSTABLE — based on roll
-    int CCD_ITERATION = 5; // number of iteration for ccd execution
+    int CCD_ITERATION = 200; // number of iteration for ccd execution
 
-    // //Weight distribution among link rotation for CCD — W_3 > W_2 > W_1 ; should be like this when adjusting
-    // double W_1 = 1; // weight for link 1 rotation
-    // double W_2 = 1; // weight for link 2 rotation
-    // double W_3 = 1; // weight for link 3 rotation
-
-    double currentServoAngle_1 = servo_1.read();
-    double currentServoAngle_2 = servo_2.read();
-    double currentServoAngle_3 = servo_3.read();
-    double currentServoAngle_4 = servo_4.read();
-    double currentServoAngle_5 = servo_5.read();
-    double currentServoAngle_6 = servo_6.read();
-    
-    Serial.print("Angle 1: ");Serial.println(currentServoAngle_1);
-    Serial.print("Angle 2: ");Serial.println(currentServoAngle_2);
-    Serial.print("Angle 3: ");Serial.println(currentServoAngle_3);
-    Serial.print("Angle 4: ");Serial.println(currentServoAngle_4);
-    Serial.print("Angle 5: ");Serial.println(currentServoAngle_5);
-    Serial.print("Angle 6: ");Serial.println(currentServoAngle_6);
-
+    //current servo angle array
+    double currentServoAngle[6];
+    for(int i = 0; i < 6; i++){
+      currentServoAngle[i] = servos[i].read();
+      Serial.print("Servo Angle ");Serial.print(i);Serial.print(" : ");Serial.println(currentServoAngle[i]);
+    }
 
     String input = Serial.readStringUntil('\n');
     input.trim();
@@ -120,15 +80,16 @@ void loop(){
       gripperStrength = input.substring(idx6 + 1, idx7).toDouble();
       mission_accomplish = input.substring(idx7 + 1).toDouble();
 
-    //All calculation for Cyclic Coordinate Descent — CCD wil be done on x-z plane
 
-    //Turns input from VLA into unit vector and scaled by VLA_ARM_PROPORTIONAL_GAIN
+
+    //All calculation for Cyclic Coordinate Descent — CCD wil be done on x-z plane
+    //Turns input from VLA into vector and scale by VLA_ARM_PROPORTIONAL_GAIN
     Vector2D vectorVLA = Vector2D(dx,dz).scalarMultiply(VLA_ARM_PROPORTIONAL_GAIN);
 
     //calculate all vectors based on the angle of each servo
-    double theta_2_rad = currentServoAngle_2 * M_PI / 180;
-    double theta_3_rad = currentServoAngle_3 * M_PI / 180;
-    double theta_4_rad = currentServoAngle_4 * M_PI / 180;
+    double theta_2_rad = currentServoAngle[1] * M_PI / 180;
+    double theta_3_rad = currentServoAngle[2] * M_PI / 180;
+    double theta_4_rad = currentServoAngle[3] * M_PI / 180;
 
     //All link vectors calculation
     Vector2D link1 = Vector2D( -l1*cos(theta_2_rad) , l1*sin(theta_2_rad) );
@@ -136,96 +97,99 @@ void loop(){
     Vector2D link3 = Vector2D( l3 * cos(theta_4_rad + theta_3_rad - theta_2_rad) , l3 * sin(theta_4_rad + theta_3_rad - theta_2_rad));
     Vector2D endEffectorVector = link1.add(link2).add(link3);
 
-    //fixing target point for CCD loop
+    //fixing target point for CCD loop, nigga im lazy
     double targetPoint_x = endEffectorVector.x + vectorVLA.x;
     double targetPoint_y = endEffectorVector.y + vectorVLA.y;
     
     Serial.print("current end effector: "); Serial.print(endEffectorVector.x);Serial.print(" , ");Serial.println(endEffectorVector.y);
     Serial.print("target point: "); Serial.print(targetPoint_x);Serial.print(" , ");Serial.println(targetPoint_y);
 
-    for (int i = 0 ; i < CCD_ITERATION; i++){
+    //start CCD from link 3 and regress down to link 1
+    int operatingLink = 3;
+    double marginOfError = vectorVLA.getMagnitude() * 0.01; // 1 percent error allowed
 
-      //calculate all vectors based on the angle of each servo
-      theta_2_rad = currentServoAngle_2 * M_PI / 180;
-      theta_3_rad = currentServoAngle_3 * M_PI / 180;
-      theta_4_rad = currentServoAngle_4 * M_PI / 180;
+    for (int i = 0 ; i < CCD_ITERATION; i++){
+      //rad from servo degree
+      theta_2_rad = currentServoAngle[1] * M_PI / 180;
+      theta_3_rad = currentServoAngle[2] * M_PI / 180;
+      theta_4_rad = currentServoAngle[3] * M_PI / 180;
 
       //All link vectors calculation
-      Vector2D link1 = Vector2D( -l1*cos(theta_2_rad) , l1*sin(theta_2_rad) );
-      Vector2D link2 = Vector2D( l2*cos( M_PI_2 - theta_2_rad + theta_3_rad) , l2*sin(M_PI/2 - theta_2_rad + theta_3_rad));
-      Vector2D link3 = Vector2D( l3 * cos(theta_4_rad + theta_3_rad - theta_2_rad) , l3 * sin(theta_4_rad + theta_3_rad - theta_2_rad));
-      Vector2D endEffectorVector = link1.add(link2).add(link3);
-      
-      //Vector from each joint to target location
+      link1 = Vector2D( -l1*cos(theta_2_rad) , l1*sin(theta_2_rad) );
+      link2 = Vector2D( l2*cos( M_PI_2 - theta_2_rad + theta_3_rad) , l2*sin(M_PI_2 - theta_2_rad + theta_3_rad));
+      link3 = Vector2D( l3 * cos(theta_4_rad + theta_3_rad - theta_2_rad) , l3 * sin(theta_4_rad + theta_3_rad - theta_2_rad));
+      endEffectorVector = link1.add(link2).add(link3);
+    
+      //braek CCD algorithm if endeffector allign with target within margin of error
+      // Vector2D distanceCheck = Vector2D(targetPoint_x - endEffectorVector.x, targetPoint_y - endEffectorVector.y );
+      // if ( distanceCheck.getMagnitude() <= marginOfError ){
+      //   break;
+      // }
+
+      //CCD one link at a time and switch to the other
+
+      const int STATE_1 = 1 ;
+      const int STATE_2 = 2 ;
+      const int STATE_3 = 3 ;
+
+      double angle_link1_jointToTarget ;
+      double angle_link2_jointToTarget;
+      double angle_link3_jointToTarget;
+
+       //Vector from each joint to target location
       Vector2D link1_jointToTarget = Vector2D(targetPoint_x, targetPoint_y);
       Vector2D link2_jointToTarget = link1_jointToTarget.minus(link1);
       Vector2D link3_jointToTarget = link2_jointToTarget.minus(link2);
+      Vector2D joint2_toEndEffector = link2.add(link3);
 
-      //Calculate angle from links to target vector directionally
-      double angle_link1_jointToTarget = link1.angleBetweenVector(link1_jointToTarget) * 180 / M_PI;
-      double angle_link2_jointToTarget = link2.angleBetweenVector(link2_jointToTarget) * 180 / M_PI;
-      double angle_link3_jointToTarget = link3.angleBetweenVector(link3_jointToTarget) * 180 / M_PI;
-      
+      switch(operatingLink){
 
-      //simple logic for base rotation using only dy as the only servo 1 can cause changes in dy
-      currentServoAngle_1 += dy * VLA_BASE_ROTATION_GAIN;
+        case STATE_3:{
+          angle_link3_jointToTarget = link3.angleBetweenVector(link3_jointToTarget) * 180 / M_PI;
+          currentServoAngle[3] += angle_link3_jointToTarget;
+          operatingLink = 2;
+}
+        case STATE_2:{
+          angle_link2_jointToTarget = joint2_toEndEffector.angleBetweenVector(link2_jointToTarget) * 180 / M_PI;
+          currentServoAngle[2] += angle_link2_jointToTarget;
+          operatingLink = 1;
+        }
 
-      //CCD inverse kinematic for end effector movement
-      currentServoAngle_2 += angle_link1_jointToTarget;
-      currentServoAngle_3 += angle_link2_jointToTarget;
-      currentServoAngle_4 += angle_link3_jointToTarget;
-    
-      //gripper grab and rotation control
-      currentServoAngle_5 += roll * VLA_GRIPPER_ROTATION_GAIN;
-      currentServoAngle_6 = mapDouble(gripperStrength, -1.0, 1.0, 180.0, 90.0);
+        case STATE_1:{
+          angle_link1_jointToTarget = endEffectorVector.angleBetweenVector(link1_jointToTarget) * 180 / M_PI;
+          currentServoAngle[1] += angle_link1_jointToTarget;
+          operatingLink = 3;
+        }
+      }
 
-      //setting limit for servo angle ignore my bad coding
-      if (currentServoAngle_1 > 180) currentServoAngle_1 = 180;
-      if (currentServoAngle_2 > 180) currentServoAngle_2 = 180;
-      if (currentServoAngle_3 > 180) currentServoAngle_3 = 180;
-      if (currentServoAngle_4 > 180) currentServoAngle_4 = 180;
-      if (currentServoAngle_5 > 180) currentServoAngle_5 = 180;
-      if (currentServoAngle_6 > 180) currentServoAngle_6 = 180;
-
-      if (currentServoAngle_1 < 0) currentServoAngle_1 = 0;
-      if (currentServoAngle_2 < 0) currentServoAngle_2 = 0;
-      if (currentServoAngle_3 < 0) currentServoAngle_3 = 0;
-      if (currentServoAngle_4 < 0) currentServoAngle_4 = 0;
-      if (currentServoAngle_5 < 0) currentServoAngle_5 = 0;
-      if (currentServoAngle_6 < 0) currentServoAngle_6 = 0;
-
-      // Vector2D distanceEndToTarget = Vector2D(targetPoint_x - endEffectorVector.x, targetPoint_y - endEffectorVector.y);
-      // if (distanceEndToTarget.getMagnitude() < 5) break;
+      //capping servo angle between 0 and 180
+      for (int i =1; i < 4; i++ ){
+        (currentServoAngle[i] > 180) ? currentServoAngle[i] = 180: currentServoAngle[i];
+        (currentServoAngle[i] < 0) ? currentServoAngle[i] = 0: currentServoAngle[i];
+      }
     }
 
-    servo_1.write(currentServoAngle_1);
-    servo_2.write(currentServoAngle_2);
-    servo_3.write(currentServoAngle_3);
-    servo_4.write(currentServoAngle_4);
-    servo_5.write(currentServoAngle_5);
-    servo_6.write(currentServoAngle_6);
+    //simple logic for base rotation using only dy as the only servo 1 can cause changes in dy
+    currentServoAngle[0] += dy * VLA_BASE_ROTATION_GAIN;
+
+    //gripper grab and rotation control
+    currentServoAngle[4] += roll * VLA_GRIPPER_ROTATION_GAIN;
+    currentServoAngle[5] = mapDouble(gripperStrength, -1.0, 1.0, 180.0, 90.0);
+
+    for(int i = 0 ; i< 6; i++){
+      servos[i].write(currentServoAngle[i]);
+    }
 
     //print log;
     Serial.print("dx: ");Serial.println(dx);
     Serial.print("dy: ");Serial.println(dy);
     Serial.print("dz: ");Serial.println(dz);
-    
-
     Serial.print("roll: ");Serial.println(roll);
     Serial.print("pitch: ");Serial.println(pitch);
     Serial.print("yaw: ");Serial.println(yaw);
 
     
     Serial.print("Distance from End Effector to Target: ");Serial.println(vectorVLA.getMagnitude());
-
-    //new link vectors calculation
-    link1 = Vector2D( -l1*cos(theta_2_rad) , l1*sin(theta_2_rad) );
-    link2 = Vector2D( l2*cos( M_PI_2 - theta_2_rad + theta_3_rad) , l2*sin(M_PI/2 - theta_2_rad + theta_3_rad));
-    link3 = Vector2D( l3 * cos(theta_4_rad + theta_3_rad - theta_2_rad) , l3 * sin(theta_4_rad + theta_3_rad - theta_2_rad));
-    endEffectorVector = link1.add(link2).add(link3);
-
-    Serial.print("new end effector: "); Serial.print(endEffectorVector.x);Serial.print(" , ");Serial.println(endEffectorVector.y);
-
     }
 
     else {

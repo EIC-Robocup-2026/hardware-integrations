0e3f099e81cb9d118e13f1e221ebb9527016d8de
armmy2530 <sutigran2557@gmail.com>
2025-11-06 22:55:25 +0700

    add lidar merger

--- walkie_bringup/launch/dual_lidar_hokuyo_lakibeam.launch.py
+++ walkie_bringup/launch/dual_lidar_hokuyo_lakibeam.launch.py
@@ -6,11 +6,11 @@ from launch import LaunchDescription
 from launch.actions import (EmitEvent, RegisterEventHandler)
 from launch.event_handlers import OnProcessStart
 from launch.events import matches_action
-from launch_ros.actions import Node, LifecycleNode
+from launch_ros.actions import ComposableNodeContainer, Node, LifecycleNode
 from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events.lifecycle import ChangeState
 from lifecycle_msgs.msg import Transition
-
+from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     # ========== Dual Lidar Configuration ==========
@@ -85,13 +85,58 @@ def generate_launch_description():
         output='screen'
     )
 
+
+    # ========== Lidar Merger Configuration ==========
+    dual_laser_merger_node = ComposableNodeContainer(
+        name='demo_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='dual_laser_merger',
+                plugin='merger_node::MergerNode',
+                name='dual_laser_merger',
+                parameters=[
+                    {'laser_1_topic': '/front_lidar'},
+                    {'laser_2_topic': '/back_lidar'},
+                    {'merged_scan_topic': '/merged_lidar'},
+                    {'target_frame': 'base_link'},
+                    {'laser_1_x_offset': 0.0},
+                    {'laser_1_y_offset': 0.0},
+                    {'laser_1_yaw_offset': 0.0},
+                    {'laser_2_x_offset': -0.0},
+                    {'laser_2_y_offset': 0.0},
+                    {'laser_2_yaw_offset': 0.0},
+                    {'tolerance': 0.01},
+                    {'queue_size': 5},
+                    {'angle_increment': 0.001},
+                    {'scan_time': 0.067},
+                    {'range_min': 0.01},
+                    {'range_max': 25.0},
+                    {'min_height': -1.0},
+                    {'max_height': 1.0},
+                    {'angle_min': -3.141592654},
+                    {'angle_max': 3.141592654},
+                    {'inf_epsilon': 1.0},
+                    {'use_inf': True},
+                    {'allowed_radius': 0.45},
+                    {'enable_shadow_filter': True},
+                    {'enable_average_filter': True},
+                    ],
+            )
+        ],
+        output='screen',
+    )
+
     # ========== Launch Description ==========
     ld = LaunchDescription()
     
     # Nodes
     ld.add_action(hokuyo_lifecycle_node)
     ld.add_action(lakibeam_node)
-    
+    ld.add_action(dual_laser_merger_node)
+
     # Event handlers for Hokuyo lifecycle (only if auto_start is true)
     if hokuyo_launch_config['auto_start']:
         ld.add_action(hokuyo_configure_event_handler)
